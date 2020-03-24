b42a191934baa349cba8247c2922b3c3f4a6a2f7
wildcard329 <enkiduwildman@gmail.com>
2020-03-24 15:29:51 -0700

    displaying list of friends

--- friends/src/components/FriendsDisplay.js
+++ friends/src/components/FriendsDisplay.js
@@ -1,22 +1,34 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 function FriendsDisplay() {
-
+    const [friends, setFriends] = useState([])
     useEffect(() => {
         getData();
-    })
+    }, [])
 
     const getData = () => {
         axiosWithAuth()
             .get('/api/friends/')
             .then(res => {
                 console.log(res);
+                setFriends(res.data)
+            })
+            .catch(err => {
+                console.error('Friends Display Error',err, err.message)
             })
     }
     return(
-        <></>
+        <div>
+            {friends.map(friend => (
+                <div key={friend.id}>
+                    <p>{friend.name}</p>
+                    <p>{friend.age}</p>
+                    <p>{friend.email}</p>
+                </div>
+            ))}
+        </div>
     )
 }
 
